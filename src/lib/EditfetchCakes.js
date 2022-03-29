87f524d0cd158187b4c62db94014723f4fd72293
TTh3 <68775086+TTh3@users.noreply.github.com>
2022-03-29 21:45:45 +1030

    Hello Everyone!

--- src/lib/EditfetchCakes.js
+++ src/lib/EditfetchCakes.js
@@ -4,18 +4,16 @@ const EditfetchCakes = async (AdminCakes, setAdminCCResponse) => {
   ClonedCAC.map((CAC, i) => {
     CAC.id = i;
   }); //61d95f0039a33573b3255962
-  fetch(
-    "https://api.jsonbin.io/v3/b/6201c5ec69b72261be542a7a/b/6201c5ec69b72261be542a7a",
-    {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-        "X-Bin-Versioning": "false",
-        "X-Master-Key": process.env.REACT_APP_JSON_LINK_PUBLIC_KEY,
-      },
-      body: JSON.stringify(ClonedCAC),
-    }
-  )
+  fetch("https://api.jsonbin.io/v3/b/6201c5ec69b72261be542a7a", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Bin-Versioning": "false",
+      "X-Master-Key":
+        "$2b$10$3UaB99VS1SOgUt.AEkuDdOewkf6QIpWd1O8YM4BB9wgHlDX9Fvhg.",
+    },
+    body: JSON.stringify(ClonedCAC),
+  })
     .then((response) => {
       setAdminCCResponse(["block", "Cakes Updating...", "block"]);
       return response.json();
